39c5d198fa87d3dab96d636380df170404230531
David Beale <david.beale@bealearts.co.uk>
2013-07-15 17:53:43 +0100

    Prediction directive Controller

--- src/next-boat/directive/PredictionDirective.js
+++ src/next-boat/directive/PredictionDirective.js
@@ -8,16 +8,7 @@ module.directive('nbPrediction', function(){
 		scope: {
 			predictions: '='
 		},
-		controller: function($scope)
-		{
-			$scope.minutesUntil = function(time)
-			{
-				var now = new Date();
-				var MINUTES = 60*1000;
-
-				return Math.ceil( (time.getTime()-now.getTime()) / MINUTES);
-			}
-		}
+		controller: 'PredictionDirectiveCtrl'
 	};
 
 	return directive;
---
+++ src/next-boat/directive/PredictionDirectiveCtrl.js
@@ -0,0 +1,11 @@
+
+module.controller('PredictionDirectiveCtrl', function($scope)
+{
+	$scope.minutesUntil = function(time)
+	{
+		var now = new Date();
+		var MINUTES = 60*1000;
+
+		return Math.ceil( (time.getTime()-now.getTime()) / MINUTES);
+	}
+});
