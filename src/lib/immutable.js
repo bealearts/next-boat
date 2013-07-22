e23cb54a9ce96e38b349e546e87bae97efcdd9fc
David Beale <david.beale@bealearts.co.uk>
2013-07-22 22:41:07 +0100

    Immutable.js Lib

---
+++ src/lib/immutable.js
@@ -0,0 +1,112 @@
+
+(function (global) {
+
+	if (!global.Immutable)
+	{
+		global.Immutable = {};
+
+
+		/* Immutable */
+
+
+		global.Immutable.create = function (proto, propertyObject)
+		{
+			var obj = Object.create(proto, propertyObject);
+			
+			makeImmutable(obj);
+			return obj; 
+		};
+
+
+		global.Immutable.clone = function (obj, properties)
+		{
+			var result = deepClone(obj);
+
+
+			// Modify properties
+			if (properties)
+			{
+				for (member in properties)
+				{
+					result[member] = properties[member];
+				}
+			}
+
+
+			makeImmutable(result);
+			return result;
+		};
+
+
+
+		/* Immutable Array */
+
+
+		global.Immutable.Array = function ()
+		{
+			var immutableArray = Object.create( Array.prototype );
+
+			immutableArray = (Array.apply( immutableArray, arguments ) || immutableArray);
+
+			return immutableArray;
+		};
+
+		global.Immutable.Array.isImmutable = function (array)
+		{
+
+		}
+
+		// Override mutating functions
+		var funcs = ['push', 'unshift', 'reverse', 'splice'];
+		funcs.forEach(function(name) {
+		    global.Immutable.Array.prototype[name] = function() {
+		        var clone = global.Immutable.clone(this);
+		        var args = Array.prototype.slice.call(arguments, 0);
+		        
+		        clone[name].apply(clone, args);
+		        
+		        return clone;
+		    }
+		});
+
+
+
+
+		/* PRIVATE */
+
+
+		function makeImmutable(obj)
+		{
+			Object.freeze(obj);
+		}
+
+
+		// Deep property Clone while maintining 'type'
+		function deepClone(obj)
+		{
+			return JSON.parse(JSON.stringify(obj), function(prop, value) { 
+				if (typeof(value) === 'object') 
+				{
+					if (Array.isArray(value))
+						var typed = new global.Immutable.Array();
+					else	
+						var typed = new obj.constructor;
+					
+					var typedRef = typed;
+					
+					Object.keys(value).forEach(function(prop) {
+						if (typeof(value[prop]) !== 'function')  
+							typedRef[prop] = value[prop];				
+					});
+
+					typedRef = null;
+					return typed;
+				}
+				else
+					return value; 
+			});			
+		}
+
+	}
+	
+}.call({}, this));
